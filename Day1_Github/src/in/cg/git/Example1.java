f22e3563d70b0b1ea72669d988f5eaba3770d4e0
Dell <Dell@DESKTOP-66GJR5M>
2022-04-07 22:01:30 +0530

    first commit

---
+++ Day1_Github/src/in/cg/git/Example1.java
@@ -0,0 +1,11 @@
+package in.cg.git;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO C2TC");
+		// TODO Auto-generated method stub
+
+	}
+
+}
