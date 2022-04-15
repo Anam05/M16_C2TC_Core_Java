3de3b367e306b52629b069fae24af11d70ccae9c
Dell <Dell@DESKTOP-66GJR5M>
2022-04-15 16:12:39 +0530

    first commit

---
+++ Day3_Oop's/src/com/indiabix/instanceof1/Example1.java
@@ -0,0 +1,18 @@
+package com.indiabix.instanceof1;
+class Example2
+{
+	void print()
+	{
+		System.out.println("HELLO");
+	}
+}
+
+public class Example1 extends Example2 {
+
+	public static void main(String[] args) {
+		Example1 e = new Example1();
+		System.out.println(e instanceof Example1);
+
+	}
+
+}
---
+++ Day3_Oop's/src/com/indiabix/instanceof1/package-info.java
@@ -0,0 +1,1 @@
+package com.indiabix.instanceof1;
