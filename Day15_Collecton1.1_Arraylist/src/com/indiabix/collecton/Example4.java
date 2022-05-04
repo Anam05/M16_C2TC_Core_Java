14fa5812734845208dbdfa1b3fa5e5de67e0e4f8
Dell <Dell@DESKTOP-66GJR5M>
2022-05-04 07:46:23 +0530

    first commit

--- Day15_Collecton1.1_Arraylist/src/com/indiabix/collecton/Example4.java
+++ Day15_Collecton1.1_Arraylist/src/com/indiabix/collecton/Example4.java
@@ -1,8 +1,14 @@
 package com.indiabix.collecton;
 
+import java.util.Vector;
+
 public class Example4 {
 
 	public static void main(String[] args) {
+		Vector v=new Vector();
+		v.add("Saujan");
+		v.add(11);
+		System.out.println(v);
 		
 
 	}
---
+++ Day15_Collecton1.1_Arraylist/src/com/indiabix/collecton/Example5.java
@@ -0,0 +1,19 @@
+package com.indiabix.collecton;
+
+import java.util.Stack;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+		Stack s =new Stack();
+		s.add(10);
+		s.add(20);
+		s.add(34);
+		s.add(30);
+		s.pop();
+		s.pop();
+		System.out.println(s);
+
+	}
+
+}
